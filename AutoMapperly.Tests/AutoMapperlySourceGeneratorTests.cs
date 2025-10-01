49d0d6966459a470d3baa8e83e6e742d2c14cefc
jorgenws <jorgen.wang.svendsen@piql.com>
2025-10-01 21:02:41 +0200

    Creates generic interface mappers from Mapperly
    Creates a IMapper<TIn,TOut> interface
    Uses that interface to create partial classes that implment it and
    routes the mapping to the actual Mapperly method.

---
+++ AutoMapperly.Tests/AutoMapperlySourceGeneratorTests.cs
@@ -0,0 +1,46 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace AutoMapperly.Tests
+{
+    public class AutoMapperlySourceGeneratorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var test = new Test("Vogon", 42);
+
+            var dto = new TestMapper().MapTestDto(test);
+
+            Assert.Equal(test.Text, dto.Text);
+            Assert.Equal(test.Value, dto.Value);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var test = new Test("Vogon", 42);
+
+            IMapper<Test, TestDto> mapper = new TestMapper();
+
+            var dto = mapper.Map(test);
+
+            Assert.Equal(test.Text, dto.Text);
+            Assert.Equal(test.Value, dto.Value);
+        }
+    }
+
+    public interface Map2<TIn, TOut>
+    {
+        TOut Map(TIn input);
+    }
+
+    public record TestDto(string Text, int Value);
+
+    public record Test(string Text, int Value);
+
+    [Mapper]
+    public partial class TestMapper
+    {
+        public partial TestDto MapTestDto(Test test);
+    }
+}
---
+++ AutoMapperly/AutoMapperlySourceGenerator.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Riok.Mapperly.Abstractions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoMapperly
+{
+    [Generator]
+    public class AutoMapperlyInterfaceSourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(ctx =>
+            {
+                ctx.AddSource("IMapper.g.cs", @"
+namespace AutoMapperly
+{
+    public interface IMapper<TInput, TOutput>
+    {
+        TOutput Map(TInput input);
+    }
+}"
+                );
+            });
+        }
+    }
+
+    [Generator]
+    public class AutoMapperlySourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var mapperInfos = context.SyntaxProvider
+                .CreateSyntaxProvider(
+                    predicate: static (node, _) => node is ClassDeclarationSyntax cds && cds.AttributeLists.Count > 0,
+                    transform: static (ctx, _) => Transform(ctx))
+                .Where(c => c is not null);
+
+            // Generate source code for each mapper info and add it to the compilation
+            context.RegisterSourceOutput(mapperInfos, (spc, mapperInfoList) =>
+            {
+                if (mapperInfoList == null || mapperInfoList.Count == 0)
+                    return;
+
+                foreach (var mapperInfo in mapperInfoList)
+                {
+                    var source = $@"
+            namespace {mapperInfo.NamespaceName}
+            {{
+            public partial class {mapperInfo.ClassName} : IMapper<{mapperInfo.InputTypeName},{mapperInfo.OutputTypeName}>
+            {{
+                public {mapperInfo.OutputTypeName} Map({mapperInfo.InputTypeName} input)
+            {{
+                    return {mapperInfo.MethodName}(input);
+            }}
+            }}
+            }}";
+                    spc.AddSource($"{mapperInfo.ClassName}_{mapperInfo.MethodName}_Mapperly.g.cs", source);
+                }
+            });
+            
+        }
+
+        private static List<MapperInfo> Transform(GeneratorSyntaxContext ctx)
+        {
+            if (!IsMapperlyClass(ctx))
+            {
+                return null;
+            }
+
+            var classDeclaration = ctx.Node as ClassDeclarationSyntax;
+            var classSymbol = ctx.SemanticModel.GetDeclaredSymbol(classDeclaration) as INamedTypeSymbol;
+
+            var namespaceName = classSymbol.ContainingNamespace?.ToDisplayString();
+
+            var methods = classDeclaration.Members
+                .OfType<MethodDeclarationSyntax>()
+                .Where(m => m.Modifiers.Any(mod => mod.IsKind(Microsoft.CodeAnalysis.CSharp.SyntaxKind.PartialKeyword)));
+
+            var methodSymbols = classSymbol.GetMembers()
+                .OfType<IMethodSymbol>()
+                .Where(m => m.IsPartialDefinition);
+
+            var mapperInfo = methodSymbols.Select(m =>
+            new MapperInfo {
+                NamespaceName = namespaceName,
+                ClassName = classSymbol.Name,
+                MethodName = m.Name,
+                InputTypeName = m.Parameters.FirstOrDefault()?.Type.ToDisplayString(),
+                OutputTypeName = m.ReturnType.ToDisplayString()
+                }
+            ).ToList();
+
+            return mapperInfo;
+        }
+
+        private static bool IsMapperlyClass(GeneratorSyntaxContext ctx)
+        {
+            var classSymbol = ctx.SemanticModel.GetDeclaredSymbol(ctx.Node as ClassDeclarationSyntax);
+
+            return classSymbol?
+                    .GetAttributes()
+                    .Any(a => a.AttributeClass?.Name == nameof(MapperAttribute))
+                    ?? false;
+        }
+
+        public class MapperInfo
+        {
+            public string NamespaceName { get; set; }
+            public string ClassName { get; set; }
+            public string MethodName { get; set; }
+            public string InputTypeName { get; set; }
+            public string OutputTypeName { get; set; }
+        }
+    }
+}
